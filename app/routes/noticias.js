eed910ea0940dc59bfe4106723b0a7b84c858b41
Tiago Soares <tiago-s@outlook.com>
2019-11-25 18:04:04 -0300

    engine mysql, connecxao com banco de dados e visualizacao dos dados na view

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,24 @@
 module.exports = (app) =>{
 
-	app.get('/noticias', (req, res) => {
-		res.render('noticias/noticias')
-	})
+	app.get('/noticias', function(req, res) {
+
+		var mysql = require('mysql')
+
+		var connection = mysql.createConnection({
+			host: '127.0.0.1',
+			user: 'root',
+			password: '',
+			database: 'portal_noticias'
+		})
+
+		
+
+		connection.query('select * from noticias', function(error, result){
+			
+			res.render('noticias/noticias', {noticias: result})
+
+		})
+
+	});
 
 }
